971214197230ff8a29576e337047e863214fdeca
Resi Respati <resir014@gmail.com>
2020-09-07 15:01:48 +0700

    deps upgrade + lint fixes (2020-09-07) (#16)

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,11 +2,7 @@ import * as React from 'react'
 
 import styles from './Container.module.scss'
 
-export const Container: React.SFC<React.HTMLProps<HTMLDivElement>> = ({
-  style,
-  children,
-  ...rest
-}) => (
+export const Container: React.SFC<React.HTMLProps<HTMLDivElement>> = ({ style, children, ...rest }) => (
   <div className={styles.container} style={style} {...rest}>
     {children}
   </div>
